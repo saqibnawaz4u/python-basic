99148571b034dcd33c92cfac15b0b1f6fa39b9bd
saqibnawaz4u <61900290+saqibnawaz4u@users.noreply.github.com>
2021-06-23 15:19:33 +0500

    initial commit

---
+++ Function.py
@@ -0,0 +1,43 @@
+def add():
+    x=12
+    y=13
+    print(x+y)
+
+
+add()
+
+def sub():
+    x=34
+    y=12
+    print(x-y)
+
+sub()
+
+def mul():
+    x= 2
+    y=3
+    print(x*y)
+
+mul()
+
+x="this is my first practice of python function"
+
+def practice():
+    print(x)
+
+practice()
+
+def div():
+    x=23
+    y=2
+    print(x/y)
+
+div()
+
+def concatinate():
+    x='sheri'
+    y='Ali'
+    print(x+y)
+
+concatinate()
+
